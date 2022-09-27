22ab5863f18d0cd773edec7ce0d8761b89f562fe
Ricardo Camacho <rscamachodev@gmail.com>
2022-09-27 11:17:47 -0300

    Exemplos originais

---
+++ src/BubbleSortV0.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class BubbleSortV0 {
+  public static void main (String args []) {
+    int X[] = new int [5];
+    int n,i,aux;
+    Scanner entrada = new Scanner (System.in);
+    for (i=0; i<=4; i++) {
+      System.out.println("Digite o "+(i+1) +"o numero: " );
+      X[i] = entrada.nextInt();
+    }
+
+    for (n=1; n<=5; n++) {
+      for (i=0; i<=3; i++) {
+        if(X[i] > X[i+1]) {
+          aux = X[i];
+          X[i] = X[i+1];
+          X[i+1] = aux;
+        }
+      }
+    }
+    for (i=0;i<=4;i++) {
+      System.out.println((i+1)+"o numero: " + X[i]);
+    }
+  }
+}
---
+++ src/BubbleSortV2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class BubbleSortV2 {
+  public static void main (String args []) {
+    int X[] = new int [5];
+    int n,i,aux,troca;
+    Scanner entrada = new Scanner (System.in);
+    for (i=0; i<=4; i++) {
+      System.out.println("Digite o "+(i+1) +"o numero: " );
+      X[i] = entrada.nextInt();
+    }
+
+    n=1;
+    troca=1;
+    while (n<=5 && troca ==1) {
+      troca=0;
+      for (i=0; i<=3; i++) {
+        if(X[i] < X[i+1]) {
+          troca = 1;
+          aux = X[i];
+          X[i] = X[i+1];
+          X[i+1] = aux;
+        }
+      }
+      n=n+1;
+    }
+    for (i=0;i<=4;i++) {
+      System.out.println((i+1)+"o numero: " + X[i]);
+    }
+  }
+}
