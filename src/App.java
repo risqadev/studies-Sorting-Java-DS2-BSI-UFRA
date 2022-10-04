5626d5059801de48a0f0bd29b1fabd2f348692bf
Ricardo Camacho <rscamachodev@gmail.com>
2022-10-04 18:10:37 -0300

    selection sort

--- src/App.java
+++ src/App.java
@@ -4,23 +4,27 @@ public class App {
     // int lista[] = Entrada.pedir(n);
     
     // int lista[] = {1,2,3,4,5};      // melhor caso
-    int lista[] = {5,4,3,2,1};      // pior caso
+    int lista[] = {3,4,5,2,1};      // pior caso
 
     // Imprimir.array(lista);
     
     // BubbleSortV2.ordenar(lista);
     // Imprimir.array(lista);
     
-    int lista1[] = BubbleSortV0.ordenar(lista.clone());
-    System.out.printf("         Lista ordenada: ");
-    Imprimir.array(lista1);
+    // int lista1[] = BubbleSortV0.ordenar(lista.clone());
+    // System.out.printf("         Lista ordenada: ");
+    // Imprimir.array(lista1);
 
-    int lista2[] = BubbleSortV1.ordenar(lista.clone());
-    System.out.printf("         Lista ordenada: ");
-    Imprimir.array(lista2);
+    // int lista2[] = BubbleSortV1.ordenar(lista.clone());
+    // System.out.printf("         Lista ordenada: ");
+    // Imprimir.array(lista2);
 
-    int lista3[] = BubbleSortV2.ordenar(lista.clone());
+    // int lista3[] = BubbleSortV2.ordenar(lista.clone());
+    // System.out.printf("         Lista ordenada: ");
+    // Imprimir.array(lista3);
+
+    int lista4[] = SelectionSort.ordenar(lista.clone());
     System.out.printf("         Lista ordenada: ");
-    Imprimir.array(lista3);
+    Imprimir.array(lista4);
   }
 }
